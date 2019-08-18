05d38c2b86154ac231e932f095a076d026eb13e4
M.C. van den Boogaart <scola@kvdb.name>
2019-08-18 15:14:01 +0200

    Add options to get list widget

--- src/worker/builder/view/snippet/widget/get-list.js
+++ src/worker/builder/view/snippet/widget/get-list.js
@@ -1,6 +1,80 @@
 import { Widget } from '../widget';
 
 export class GetList extends Widget {
+  constructor(options = {}) {
+    super(options);
+
+    this._count = null;
+    this._offset = null;
+    this._search = null;
+
+    this.setCount(options.count);
+    this.setOffset(options.offset);
+    this.setSearch(options.search);
+  }
+
+  getCount() {
+    return this._count;
+  }
+
+  setCount(value = null) {
+    this._count = value;
+    return this;
+  }
+
+  count(value) {
+    return this.setCount(value);
+  }
+
+  getOffset() {
+    return this._offset;
+  }
+
+  setOffset(value = null) {
+    this._offset = value;
+    return this;
+  }
+
+  offset(value) {
+    return this.setOffset(value);
+  }
+
+  getSearch() {
+    return this._search;
+  }
+
+  setSearch(value = null) {
+    this._search = value;
+    return this;
+  }
+
+  search(value) {
+    return this.setSearch(value);
+  }
+
+  buildParams() {
+    const names = ['count', 'offset', 'search'];
+
+    const expanded = names.filter((name) => {
+      return this['_' + name] !== null;
+    }).map((name) => {
+      return `${name}=${this['_' + name]}`;
+    }).join('&');
+
+    const expand = names.filter((name) => {
+      return this['_' + name] === null;
+    }).join(',');
+
+    let string = expanded;
+
+    if (expand) {
+      string += string ? '&' : '';
+      string += `{${expand}}`;
+    }
+
+    return string;
+  }
+
   buildWidget(args) {
     const b = this._builder;
 
@@ -9,8 +83,10 @@ export class GetList extends Widget {
         (index < this._name.length - 1 ? `/%(${name}_id)s` : '');
     }).join('');
 
+    const params = this.buildParams();
+
     return b.request().resource(
-      `GET ${resource}?{count,offset,search}`
+      `GET ${resource}?${params}`
     ).indicator(
       b.selector('.loading')
     ).act(
